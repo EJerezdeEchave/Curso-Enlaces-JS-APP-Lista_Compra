9050e09c230d10ebfbdd25d365cb48ba93245d9a
EJerezdeEchave <e.jerezdeechave@gmail.com>
2019-03-19 21:16:48 +0100

    Creados el index.html y main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//(1)definicion de variables
+const articulo = document.getElementById("articulo");
+const cantidad = document.querySelector("#cantidad");
+const prioridad = document.querySelector("#prioridad");
+const agregar = document.querySelector("#agregar");
+//(2)definicion de funciones
+
+//(3)definicion de eventos
